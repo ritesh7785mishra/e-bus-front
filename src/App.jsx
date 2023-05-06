bc698b813a55311a3b3569dc0bd56450510def8b
unknown <ritesh7785mishra@gmail.com>
2023-05-06 15:00:54 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        style={{
+          color: "black",
+        }}
+      >
+        I am here
+      </div>
+    </>
+  );
+}
+
+export default App;
